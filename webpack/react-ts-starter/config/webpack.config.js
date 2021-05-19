db11af671b7a2e6da62997d5463ddc65f6e902f4
shanmugharajk <shanmugharaj.k@outlook.com>
2021-05-19 19:31:24 +0530

    feat(webpack): conditional configuration

--- webpack/react-ts-starter/config/webpack.config.js
+++ webpack/react-ts-starter/config/webpack.config.js
@@ -2,11 +2,37 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
+// == env utils ==
+const env = (name) => name === (process.env.NODE_ENV || "development");
+
+const isProd = () => env("production");
+
+const ifProd = (t, f) => (isProd() ? t : f);
+
+// == paths ==
+const dist = path.resolve(__dirname, "..", "./dist");
+const context = path.resolve(__dirname, "..", "./src");
+const configFile = "../tsconfig.json";
+const main = process.env.MAIN_ENTRY || "./index.tsx";
+
 module.exports = {
-  entry: path.resolve(__dirname, "..", "./src/index.tsx"),
+  context,
+
+  entry: main,
+
+  output: {
+    path: dist,
+    filename: ifProd("[name].[chunkhash].js", "[name].js"),
+    chunkFilename: ifProd("[name].[chunkhash].js", "[name].js"),
+  },
+
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
+    alias: {
+      "~": ".",
+    },
   },
+
   module: {
     rules: [
       {
@@ -20,21 +46,23 @@ module.exports = {
       },
     ],
   },
-  output: {
-    path: path.resolve(__dirname, "..", "./build"),
-    filename: "bundle.js",
-  },
+
   mode: "development",
+
   plugins: [
-    // Does the type checking
+    // Does the type checking since babel only compiles to Javscript.
     new ForkTsCheckerWebpackPlugin({
       typescript: {
-        configFile: path.resolve(__dirname, "..", "./tsconfig.json"),
+        configFile,
       },
     }),
+
     // injects the bundle.js file
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "..", "./src/index.html"),
+      template: "index.html",
+      favicon: "favicon.ico",
     }),
   ],
+
+  devtool: ifProd(undefined, "eval-cheap-module-source-map"),
 };
